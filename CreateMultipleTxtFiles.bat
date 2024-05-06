2555e312c77cde5d9ba8d3acc65956a7c7ae6d17
Ngoc Vu Vo <31318434+vuvo151@users.noreply.github.com>
2024-05-06 09:29:04 +0900

    Create CreateMultipleTxtFiles.bat

---
+++ CreateMultipleTxtFiles.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Creating txt files...
+echo. > txt1.txt
+echo. > txt2.txt
+echo. > txt3.txt
+echo. > txt4.txt
+echo. > txxt5.txt
+echo. > ttt6.txt
+echo Txt files created successfully.
